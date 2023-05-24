809b416a7d678c284f16cbc2a78ac0acee03bee4
NehemieMbg <nehemie.mbg@gmail.com>
2023-05-24 22:54:06 +0200

    hot fix

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
+"""Concate two string together"""
+
+
 def concat(str1: str, str2: str) -> str:
+    """Return the string"""
     return str1 + str2
--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 import math
+"""Return the floor of a float"""
+
 
 def floor(n: float) -> int:
+	"""Return the floor of a float"""
     return math.floor(n)
