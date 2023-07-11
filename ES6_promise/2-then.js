ea03b9d45e08ca948514c9f92bc193a9351a21ef
NehemieMbg <nehemie.mbg@gmail.com>
2023-07-11 10:16:41 +0200

    Catch

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => body)
+    .then((value) => value)
     .catch((error) => error)
     .finally(() => console.log('Got a response from the API'));
 }
