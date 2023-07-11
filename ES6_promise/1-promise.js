fc7f480a54006d11219602b13a78723f06337320
NehemieMbg <nehemie.mbg@gmail.com>
2023-07-11 10:07:33 +0200

    Promise

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,9 +1,6 @@
 export default function getFullResponseFromAPI(success) {
-  if (success)
-    return {
-      status: 200,
-      body: 'Success',
-    };
-
-  if (!success) throw new Error('The fake API is not working currently');
+  return new Promise((resolve, reject) => {
+    if (success) resolve({ status: 200, body: 'Success' });
+    if (!success) reject(new Error('The fake API is not working currently'));
+  });
 }
