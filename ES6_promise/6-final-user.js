50b83f2cef2fe2f1d531fe9d5540fecea750fe74
NehemieMbg <nehemie.mbg@gmail.com>
2023-07-11 11:29:17 +0200

    multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,5 +5,12 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ]).then((results) => {
+    return results.map((result) => {
+      return {
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason,
+      };
+    });
+  });
 }
