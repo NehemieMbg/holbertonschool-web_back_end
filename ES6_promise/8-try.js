78db7b728a503fd6d41dbe652c8d695026527a81
NehemieMbg <nehemie.mbg@gmail.com>
2023-07-13 11:47:50 +0200

    try catch

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,11 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    if (denominator === 0) throw Error('cannot divide by 0');
-
-    return numerator / denominator;
-  } catch (error) {
-    console.error(error);
-  }
+  if (denominator === 0) throw Error('cannot divide by 0');
+  else return numerator / denominator;
 }
 
 console.log(divideFunction(10, 2));
