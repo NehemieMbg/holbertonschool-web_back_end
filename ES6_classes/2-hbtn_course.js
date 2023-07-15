7e108e8389de0733df1d0a08923c876f3338801d
NehemieMbg <nehemie.mbg@gmail.com>
2023-07-15 16:44:41 +0200

    Getter and setter

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -2,10 +2,7 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') throw new Error('Name must be a string');
     if (typeof length !== 'number') throw new Error('Length must be a number');
-    if (
-      Array.isArray(students) &&
-      students.every((ele) => typeof ele === 'string')
-    )
+    if (Array.isArray(students))
       throw new Error('Students must be an array of strings');
 
     this._name = name;
