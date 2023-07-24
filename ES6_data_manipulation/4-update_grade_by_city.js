d4b951a6efab401bf4d980ae920a5d1bb3219d03
NehemieMbg <nehemie.mbg@gmail.com>
2023-07-24 11:44:40 +0200

    Combine

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -7,23 +7,23 @@ export default function updateStudentGradeByCity(
     return [];
   }
 
-  const updatedStudentList = studentsList
-    // Filter the array received
-    .filter((student) => student.location === city)
-    // Returns an updated version of the array with the new grades
-    .map((student) => {
-      // Find the grade object that has the same id as the student
-      const currGrade = newGrades.find((grade) => {
-        if (student.id === grade.studentId) {
-          return grade.grade;
-        }
-      });
-      // Past the onject and adds the new grade to the current object
-      return {
-        ...student,
-        grade: currGrade?.grade || 'N/A',
-      };
-    });
-
-  return updatedStudentList;
+  return (
+    studentsList
+      // Filter the array received
+      .filter((student) => student.location === city)
+      // Returns an updated version of the array with the new grades
+      .map((student) => {
+        // Find the grade object that has the same id as the student
+        const currGrade = newGrades.find((grade) => {
+          if (student.id === grade.studentId) {
+            return grade.grade;
+          }
+        });
+        // Past the onject and adds the new grade to the current object
+        return {
+          ...student,
+          grade: currGrade?.grade || 'N/A',
+        };
+      })
+  );
 }
