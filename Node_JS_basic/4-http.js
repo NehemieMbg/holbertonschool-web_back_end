99bc03e91aab08bb465e9c8e6255e9d1231aae6d
NehemieMbg <nehemie.mbg@gmail.com>
2023-07-25 17:23:27 +0200

    small server

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,7 +3,12 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else {
+    res.end('Hello Holberton School!');
+  }
 });
 
 const port = 1245;
