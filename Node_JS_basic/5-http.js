5462d2a6487154dc172fc7c8407d92ab432ad8cf
NehemieMbg <nehemie.mbg@gmail.com>
2023-07-30 16:59:57 +0200

    complex http

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const countStudent = require('./3-read_file_async');
+
+const filePath = process.argv[2];
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  }
+
+  if (req.url === '/students') {
+    res.end('This is the list of our students');
+    countStudent(filePath)
+      .then((data) => {
+        res.write(data.join('\n'));
+        res.end();
+      })
+      .catch(() => res.end('Cannot load the database'));
+  }
+});
+
+const port = 1245;
+
+app.listen(port, () => {
+  console.log(`Server is running at ${port}`);
+});
+
+module.exports = app;
