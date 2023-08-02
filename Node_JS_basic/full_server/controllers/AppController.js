c6a9ab8f33bd3a4cb5dc0d9dec4cffe4f9b4d13a
NehemieMbg <nehemie.mbg@gmail.com>
2023-08-02 19:09:17 +0200

    test

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -1,5 +1,5 @@
-export class AppController {
-  static getHomeppage(request, response) {
+class AppController {
+  static getHomepage(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Hello Holberton School!');
   }
