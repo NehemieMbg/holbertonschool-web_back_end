e3eab6ed0d5d424cc71995c4df61d97dfaf77741
NehemieMbg <nehemie.mbg@gmail.com>
2023-08-02 19:10:53 +0200

    test

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,26 +1,22 @@
 const fs = require('fs');
 
-async function readDatabase(pathname) {
-  // turn the readFile synchronous function to an ansynchronous function
-  const readFile = util.promisify(fs.readFile);
-
-  try {
-    const data = await readFile(pathname, 'utf8');
-
-    const studentData = data.split('\n').slice(1, -1);
-    const CS = studentData
-      .filter((student) => student.split(',')[3] === 'CS')
-      .map((student) => student.split(',')[0]);
-
-    const SWE = studentData
-      .filter((student) => student.split(',')[3] === 'SWE')
-      .map((student) => student.split(',')[0]);
-
-    return {
-      CS: CS,
-      SWE: SWE,
-    };
-  } catch (error) {
-    throw new Error(error);
-  }
+function readDatabase(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const lines = data.split('\n').map((line) => line.trim());
+        const result = {};
+        lines.forEach((line) => {
+          const [firstName, lastName, field] = line.split(',');
+          if (!result[field]) {
+            result[field] = [];
+          }
+          result[field].push(firstName);
+        });
+        resolve(result);
+      }
+    });
+  });
 }
